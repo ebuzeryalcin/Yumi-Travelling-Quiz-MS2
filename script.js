a4c9cff9a02fb2a56bf9412388ff975747f4776c
ebuzeryalcin <ebuzerorhan@gmail.com>
2020-12-26 23:30:59 +0000

    Added script.js file and added questions I will use for my responsive quiz.

---
+++ script.js
@@ -0,0 +1,32 @@
+// QUESTIONS with answers that I am using 
+const questions = [
+    {
+        "question": "WHAT DOES ESCAPISM MEAN TO YOU?",
+        "answer1": "Diving Into Local Culture",
+        "answer1Total": "3",
+        "answer2": "Adventure",
+        "answer2Total": "1"
+    },
+    {
+        "question": "WHAT DO YOU VALUE MOST IN YOUR ESCAPES?",
+        "answer1": "Connecting With Nature",
+        "answer1Total": "1",
+        "answer2": "Buzz Of The City",
+        "answer2Total": "3"
+    },
+    {
+        "question": "WHICH ACTIVITY WOULD BE YOUR TOP CHOICE?",
+        "answer1": "Exploring The Countryside",
+        "answer1Total": "1",
+        "answer2": "Wine Tasting",
+        "answer2Total": "3"
+    },
+    {
+        "question": "WHERE WOULD YOU ESCAPE TO?",
+        "answer1": "Itay",
+        "answer1Total": "3",
+        "answer2": "Norway",
+        "answer2Total": "1"
+    },
+    
+]
