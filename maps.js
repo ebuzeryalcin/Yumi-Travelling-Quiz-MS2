822b6a9d64451ed04a6d61c9a903209e8fda6452
ebuzeryalcin <ebuzerorhan@gmail.com>
2020-12-27 22:40:18 +0000

    Added google maps api code that I will use in result.js

---
+++ maps.js
@@ -0,0 +1,18 @@
+// Google maps function
+function initMap() {
+
+    // Locations that will show depending on score
+    const naturalLocation = [
+        { lat: 60.12527813932983, lng: 6.739055862441921 }, //norway
+        { lat: 64.15262991930668, lng: -21.98721641662982 }, // iceland
+        { lat: 66.56348882605869, lng: 25.707489226325695 }, // Finland
+    ]
+
+    const cityLocation = [
+        { lat: 41.00675900234117, lng: 28.97895254012631 }, // turkey
+        { lat: 41.902782, lng: 12.496366 }, // italy
+        { lat: 51.516168365645285, lng: -0.12975831845500385 }, // England
+    ]
+
+
+}
